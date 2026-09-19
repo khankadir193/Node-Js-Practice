11111a3d549a4ac18a2d17421f82994d71d1455e
khankadir193 <abdulkadirk059@gmail.com>
2026-09-19 17:11:03 +0530

    feat: add database configuration and setup with dotenv, include db connection in services

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('hello abdul kadir khan');
+import express from 'express';
+import db from './Services/db.js';
+
+const app = express();
+
+app.get('/',async (req,res)=>{
+    try{
+        const result = await db.query('SELECT * FROM customers');
+        // console.log('result .....',result.rows);
+        res.json(result.rows);
+    }catch(err){
+        console.log(err);
+        res.status(500).json({message:'Database error'});
+    }
+});
+
+app.listen(5000,()=>{
+    console.log('Server running on port 5000');
+})
